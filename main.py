2e2f38bef0e8003f5598ad2db5e34412d89e5a2c
GodOfArms <mykhailo.ruzmetov@student.tuke.sk>
2022-12-03 23:46:56 +0100

    added focus analysis

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 if __name__ == '__main__':
-    request = "Web Development companies"
+    request = "job for AI developers and for data scientists"
 
     ss = SemanticSort(request)
     print(ss.sort_data(20))
--- src/semantic_sort/semantic_sort.py
+++ src/semantic_sort/semantic_sort.py
@@ -1,33 +1,34 @@
 from src.utils import ProcessData
-from src.find_synonyms import synonym_extractor
 import spacy, json
 
 
 class SemanticSort:
     def __init__(self, request: str, data_path='./data.json'):
         self.__nlp = spacy.load('en_core_web_md')
-        self.__requests = ''.join([i for i in request if i.isalpha() or i == ' '])
+        self.__request = ''.join([i for i in request if i.isalpha() or i == ' '])
         self.__data = ProcessData(data_path)
 
 
-    def __fitness_score(self, description: str) -> float:
-        sentence1 = self.__nlp(self.__requests)
-        sentence2 = self.__nlp(description)
+    def __fitness_score(self, item: str) -> float:
+        sentence1 = self.__nlp(self.__request)
+        sentence2 = self.__nlp(item)
 
         return sentence1.similarity(sentence2)
 
     
-    def sort_data(self, separator: int) -> list:
-        # descriptions = [i['description'] for i in self.__data.summary_data[:separator]]
-
-        # return list(sorted(descriptions, 
-        #               key=lambda x: self.__fitness_score(x), reverse=True))
-        
+    def sort_data(self, separator: int) -> dict:
+        summary = self.__data.parse_summary()[:separator]
+        focus_vals = [i[0]['values'] for i in self.__data.parse_focus()[:separator]]
         result = {i['name']: self.__fitness_score(i['description'])
-                for i in self.__data.parse_summary()[:separator]}
+                for i in summary}
+            
+        for key, topics in zip(result, focus_vals):
+            for topic in topics:
+                result[key] += self.__fitness_score(topic['name'])/len(topics)
+
+        sorted_result = {k: v for k, v in sorted(result.items(), key=lambda item: item[1], reverse=True)}
 
         with open('result.json', 'w') as fp:
-            json.dump(result, fp)
+            json.dump(sorted_result, fp)
 
-        return result
-        
+        return sorted_result
