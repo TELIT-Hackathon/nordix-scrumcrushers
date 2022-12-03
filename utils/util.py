2f2e749f6d511f9dbf816587d2db9e0ded175e41
Nikita <nikita.shalashkov@gmail.com>
2022-12-03 16:39:40 +0100

    New features

--- utils/util.py
+++ utils/util.py
@@ -1,19 +1,42 @@
-import pandas as pd
+import json
+from pprint import pprint
 
 
 class ProcessData:
 
     def __init__(self, filepath="../data.json"):
-        self.__filepath = filepath
+        self.__filepath: str = filepath
         self.data = self.__open()
         self.focus_data = self.__parse_focus()
         self.portfolio_data = self.__parse_portfolio()
+        self.reviews_data = self.__parse_reviews()
+        self.summary_data = self.__parse_summary()
+        self.scores = self.__parse_scores_and_description()
 
-    def __open(self) -> pd.DataFrame:
-        return pd.read_json(self.__filepath)
+    def __open(self) -> dict:
+        try:
+            with open(self.__filepath, "r", encoding="utf-8") as f:
+                return json.load(f)
 
-    def __parse_focus(self) -> pd.Series:
-        return self.data.get("focus")
+        except Exception as e:
+            print(e)
 
-    def __parse_portfolio(self) -> pd.Series:
-        return self.data.get("portfolio")
+    def __parse_focus(self) -> dict:
+        return {idx: el["focus"] for idx, el in enumerate(self.data)}
+
+    def __parse_portfolio(self) -> dict:
+        return {idx: el["portfolio"] for idx, el in enumerate(self.data)}
+
+    def __parse_reviews(self) -> dict:
+        return {idx: el["reviews"] for idx, el in enumerate(self.data)}
+
+    def __parse_summary(self) -> dict:
+        return {idx: el["summary"] for idx, el in enumerate(self.data)}
+
+    def __parse_scores_and_description(self) -> dict:
+        return {el["name"]: (el["rating"], el["description"])
+                for el in self.summary_data.values()}
+
+
+process = ProcessData()
+pprint(type(process.scores))
