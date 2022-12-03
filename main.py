1a2816e86ebcb846e8257cab9b7f940a73c578fc
GodOfArms <mykhailo.ruzmetov@student.tuke.sk>
2022-12-03 15:38:44 +0100

    changed functionality

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from utils import ProcessData
 if __name__ == '__main__':
     description = 'Seven Peaks is a world-class technology company specializing in digital transformation for companies worldwide, delivering scalable software solutions and experience design, aimed at driving business impact.\nWe are based in Bangkok, Thailand, with a growing team of more than 200 industry professionals from over 25 different countries currently providing engineering-led services in native mobile app development, web technology, UX/UI design and more.\n \nSeven Peaks recently acquired Morphosis, one of the leading UX/UI design companies in Thailand which is now part of the Seven Peaks group.\nFor more information please visit https://sevenpeakssoftware.comRead more...'
     request = "Introduce your business and what you do there.\n\nI'm the founder and CEO of Riskwolf, a Zurich-based insurtech startup."
-    
+
     sa = SemanticAnalyzer(description, request)
     print(sa.fitness_score())
--- sa/semantic_analyzer.py
+++ sa/semantic_analyzer.py
@@ -1,7 +1,6 @@
 class SemanticAnalyzer:
-    def __init__(self, description: str, request: str):
+    def __init__(self, request: str):
         self.__abc = "abcdefghijklmnopqrstuvwxyz "
-        self.__description = self.__preprocess_text(description)
         self.__request = self.__preprocess_text(request)
     
 
@@ -10,7 +9,8 @@ class SemanticAnalyzer:
         text = ''.join([c for c in text if c in self.__abc])
 
         return text.split(' ')
-    
 
-    def fitness_score(self) -> int:
-        return len(set(self.__description) & set(self.__request))
+
+    def fitness_score(self, description: str) -> int:
+        tmp_description = self.__preprocess_text(description)
+        return len(set(tmp_description) & set(self.__request))
