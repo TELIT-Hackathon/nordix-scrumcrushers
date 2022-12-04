5d4dbf9437882107f404562a85efe0aab244f8ec
Nikita <nikita.shalashkov@gmail.com>
2022-12-04 03:14:38 +0100

    Create getInput.py

---
+++ src/js/getInput.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route('/')
+def render():
+    return render_template("index.html")
+
+
+@app.route('#welcome', methods=['GET'])
+def input_data() -> str:
+    data = request.input_stream['input_name']
+    return str(data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
